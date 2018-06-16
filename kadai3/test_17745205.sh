05af2c993e0589222bb5225d66711b14a78ad958
0oSco0 <shengchen.jp@gmail.com>
2018-06-16 06:06:22 +0000

    2018.6.16 2

--- kadai3/test_17745205.sh
+++ kadai3/test_17745205.sh
@@ -25,10 +25,13 @@ rm -f $tmp-*
 echo "Zero And One Is not Prime" > $tmp-ans
 ./kadai3/17745205.sh 0 > $tmp-out || ERROR_EXIT "TEST 0 input"
 diff $tmp-ans $tmp-out || ERROR_EXIT "TEST 0 output"
-./kadai3/17745205.sh 1 > $tmp-out || ERROR_EXIT "TEST 1 input"
-diff $TMP-ans $Tmp-out || ERROR_EXIT "TEST 1 output"
+echo "0 Ok"
+rm -f $tmp-*
 
-echo "0 OK"
+echo "Zero And One Is not Prime"> $tmp-ans
+./kadai3/17745205.sh 1 > $tmp-out || ERROR_EXIT "TEST 1 input"
+diff $tmp-ans $tmp-out || ERROR_EXIT "TEST 1 output"
+echo "1 OK"
 rm -f $tmp-*
 
 exit 0
